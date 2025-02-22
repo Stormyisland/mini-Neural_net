431f58ae3764f006da6087d1066b1af1081c91a8
Stormyisland <jacqueslavoie2015@gmail.com>
2025-02-22 01:12:11 -0800

    first commit

--- layer.py
+++ layer.py
@@ -4,6 +4,13 @@ import numpy as np
 # Represents a lsyer of  the madel
 class Layer:
     def __init__(self, input_size, output_size, alpha, batch_size, bias=False, activation_func=None):
-        pass 
+        self.input_size = input_size
+        self.output_size = output_size
+        self.alpha = alpha 
+        self.batch_size = batch_size
+        self.activation_func = activation_func
+        
+        self.weights = np.random.rand((self.input_size, self.output_size, ))
+        
 
 
