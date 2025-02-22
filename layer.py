520d11e0e125e6c94de88b90b13c4d155a7b27ba
Stormyisland <jacqueslavoie2015@gmail.com>
2025-02-22 01:04:28 -0800

    first commit

---
+++ layer.py
@@ -0,0 +1,9 @@
+import numpy as np 
+
+
+# Represents a lsyer of  the madel
+class Layer:
+    def __init__(self, input_size, output_size, alpha, batch_size, bias=False, activation_func=None):
+        pass 
+
+
