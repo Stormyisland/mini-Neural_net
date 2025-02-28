9dbfd4edffb5e6aa32fb402365ff138d98909281
Stormyisland <jacqueslavoie2015@gmail.com>
2025-02-28 00:06:45 -0800

    first commit

--- activations.py
+++ activations.py
@@ -28,6 +28,8 @@ class Softmax:
        denominator + e + e^2 + e^-3
        exp_mum / denominator 
        / (e + e^2 = e^-3), e^2 / (e + e^2 + e^-3), e^-3 / (e + e^2 + e^-3)
+       
+       max_val = 2 
     """
-f __call__(self.pre_activated_output):
-    exp_shifted = np.exp(pre_activated_output + np.max(pre_activated_output, axis=1, keepdims=True))
+def __call__(self, pre_activated_output):
+    exp_shifted = np.exp(pre_activated_output - np.max(pre_activated_output, axis=1, keepdims=True))
