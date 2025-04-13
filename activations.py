61eced4a234629a7b1a4fa7576e858e54262c6b1
Stormyisland <jacqueslavoie2015@gmail.com>
2025-04-12 20:55:57 -0700

    first commit

--- activations.py
+++ activations.py
@@ -5,7 +5,7 @@ import numpy as np
 
 class ReLU:
     """f(x)= {0  x <=0
-              x  x > 0}
+            
     """
     def __call__(self, pre_activated_output):
         return np.where(pre_activated_output <= 0, 0, 1) * pre_activated_output
