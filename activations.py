74b26acd81bfb7c2f25af66eb92824eda8fb472d
Stormyisland <jacqueslavoie2015@gmail.com>
2025-04-19 12:06:39 -0700

    first commit

--- activations.py
+++ activations.py
@@ -28,3 +28,4 @@ def __call__(self, pre_activated_output):
     exp_shifted = np.exp(pre_activated_output - np.max(pre_activated_output, axis=1, keepdims=True))
     denominator = np.sum(exp_shifted)
     return exp_shifted / denominator
+  def __call__()
